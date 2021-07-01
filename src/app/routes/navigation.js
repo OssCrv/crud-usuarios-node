ee4d3fb57d5338e434b512f47bebe3dae40c895a
Ossman <carvajalossman@gmail.com>
2021-07-01 14:20:10 -0500

    Falta el create y mejorar los templates

--- src/app/routes/navigation.js
+++ src/app/routes/navigation.js
@@ -1,7 +1,8 @@
 const connection = require('../../config/dbConnection');
 const bcryptjs = require("bcryptjs");
 
-module.exports = app => {    
+module.exports = app => { 
+    //Ruta principal   
     app.get('/', (req, res) => {
         if (req.session.isLoggedIn){
             connection.query("SELECT * FROM users WHERE rol != 'admin'", (err,results) => {
@@ -15,15 +16,56 @@ module.exports = app => {
         }
         
     })
+    
+    //Ruta de usuarios activos
+    app.get('/active', (req,res) => {
+        if (req.session.isLoggedIn){
+            connection.query("SELECT * FROM users WHERE is_active != 0 AND rol = 'client'", (err,results) => {
+                res.render("../views/pages/active", {
+                    login: true,
+                    userList: results
+                });
+            })
+        } else {
+            res.redirect("/login");
+        }
+    })
 
-
+    //Ruta login
     app.get('/login', (req, res) => {
         res.render("../views/pages/login");
     })
     
+    //Logout sesion
     app.get('/logout', (req,res) =>{
         req.session.destroy(() => {
             res.redirect('/');
+        })
+    })
+
+    //Delete tabla usuarios
+    app.get('/delete/:id', (req, res) => {
+        const id = req.params.id;
+        connection.query('DELETE FROM users WHERE cedula = ?', [id], (err, results) => {
+            if(err){
+                console.log(err)
+            } else {
+                res.redirect('/');
+            }
+        })
+    })
+
+    //Edit tabla usuarios
+    app.post('/edit/:id', (req, res) => {
+        const id = req.params.id;
+        const {cedula, full_name, user_name, is_active} = req.body;
+        connection.query('UPDATE users SET full_name = ?, user_name = ?, is_active = ? WHERE cedula = ?', 
+        [full_name, user_name, is_active, id], (err,results) => {
+            if(err){
+                console.log(err);
+            } else {
+                res.redirect("/");
+            }
         })
     })
     //Metodos post
